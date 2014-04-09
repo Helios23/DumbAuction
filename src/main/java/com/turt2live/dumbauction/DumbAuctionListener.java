4e96fa0e387a530ab98c384a59bc84c2eb8ac26a
turt2live <travpc@gmail.com>
2014-04-08 19:09:35 -0600

    Add untested world change code

--- src/main/java/com/turt2live/dumbauction/DumbAuctionListener.java
+++ src/main/java/com/turt2live/dumbauction/DumbAuctionListener.java
@@ -7,6 +7,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 /**
  * Listener for the Bukkit functions of DumbAuction
@@ -22,10 +23,26 @@ public class DumbAuctionListener implements Listener {
         Player player = event.getPlayer();
         if (!player.hasPermission("dumbauction.admin") && !plugin.getConfig().getBoolean("auctions.allow-gamemode-change", false)) {
             Auction auction = plugin.getAuctionManager().getActiveAuction();
+            if (auction == null) return;
             if (auction.getRealSeller().equalsIgnoreCase(player.getName())
                     || (auction.getHighestBid() != null && auction.getHighestBid().getRealBidder().equalsIgnoreCase(player.getName()))) {
                 event.setCancelled(true);
                 plugin.sendMessage(player, ChatColor.RED + "You cannot change game modes right now.");
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onWorldChange(PlayerTeleportEvent event) {
+        Player player = event.getPlayer();
+        if (event.getTo().getWorld().equals(event.getFrom().getWorld())) return;
+        if (!player.hasPermission("dumbauction.admin") && !plugin.getConfig().getBoolean("auctions.allow-world-change", true)) {
+            Auction auction = plugin.getAuctionManager().getActiveAuction();
+            if (auction == null) return;
+            if (auction.getRealSeller().equalsIgnoreCase(player.getName())
+                    || (auction.getHighestBid() != null && auction.getHighestBid().getRealBidder().equalsIgnoreCase(player.getName()))) {
+                event.setCancelled(true);
+                plugin.sendMessage(player, ChatColor.RED + "You cannot change worlds right now.");
             }
         }
     }
